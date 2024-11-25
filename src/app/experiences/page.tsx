19055bfa6c2dee20d1e9a0139b207c99dea07530
jamwalsudip <sudipsjamwal@gmail.com>
2024-11-25 17:40:25 +0530

    Bugs around the pdf view option

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -12,6 +12,7 @@ const ExperiencesPage = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        overflow: "auto",
         position: "fixed",
         top: 0,
         left: 0,
@@ -21,19 +22,21 @@ const ExperiencesPage = () => {
     >
       <div
         style={{
-          width: "595px", // This is equivalent to 21cm in pixels
-          height: "1048px", // This is equivalent to 37cm in pixels
-          maxWidth: "90vw",
-          maxHeight: "90vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          width: "100%",
+          maxWidth: "800px",
+          minWidth: "300px",
+          margin: "20px",
+          position: "relative",
+          paddingTop: "176.19%", // Maintains 21:37 aspect ratio (37/21 * 100)
         }}
       >
         <object
           data="/resume.pdf#view=FitH&scrollbar=0&toolbar=0&navpanes=0&statusbar=0"
           type="application/pdf"
           style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
             width: "100%",
             height: "100%",
             border: "none",
