5c59dcc56cf45dbd787941fc55fa97ee0376b844
jamwalsudip <sudipsjamwal@gmail.com>
2024-11-25 17:34:03 +0530

    Changes around the pdf view option

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -7,25 +7,41 @@ const ExperiencesPage = () => {
         backgroundColor: "#191919",
         margin: 0,
         padding: 0,
-        height: "100vh",
+        minHeight: "100vh",
+        width: "100%",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
       }}
     >
-      <object
-        data="/resume.pdf#view=FitH&scrollbar=0&toolbar=0&navpanes=0&statusbar=0"
-        type="application/pdf"
+      <div
         style={{
-          width: "100%",
-          maxWidth: "1200px",
-          minWidth: "300px",
-          height: "100%",
-          border: "none",
+          width: "595px", // This is equivalent to 21cm in pixels
+          height: "1048px", // This is equivalent to 37cm in pixels
+          maxWidth: "90vw",
+          maxHeight: "90vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <p>Your browser does not support PDFs.</p>
-      </object>
+        <object
+          data="/resume.pdf#view=FitH&scrollbar=0&toolbar=0&navpanes=0&statusbar=0"
+          type="application/pdf"
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "none",
+          }}
+        >
+          <p>Your browser does not support PDFs.</p>
+        </object>
+      </div>
     </div>
   );
 };
