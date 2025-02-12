418f3413adca0564160bd97a835f0057904ea26f
jamwalsudip <sudipsjamwal@gmail.com>
2025-02-12 14:13:25 +0530

    Add Google Analytics tracking to all pages using Next.js Script component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const effra = localFont({
   src: [
@@ -98,6 +99,7 @@ export default function RootLayout({
       <body className={`${effra.variable} antialiased`}>
         {children}
       </body>
+      <GoogleAnalytics />
     </html>
   );
 }
